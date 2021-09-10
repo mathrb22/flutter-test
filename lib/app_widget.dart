ac3f74741aae683e624ae7f553d3a939f5d31608
Marcus Santos <marcus.santos@uppercase.com.br>
2021-09-10 11:42:00 -0300

    chore: criando a primeira pagina e ajustando arquivo main

---
+++ lib/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:fluter_test/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Test',
+      theme: ThemeData(primaryColor: Colors.purple),
+      home: HomePage(),
+    );
+  }
+}
